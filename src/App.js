48f55b4fc26af05134b06f8902b46a8dcbe79295
Anthony Curtis <tonydc1997@gmail.com>
2018-05-26 07:51:47 -0700

    Install tachyons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
-const
+const Navigation = () => {
+  return (
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p>Sign Out</p>
+    </nav>
+  )
+}
+
+export default Navigation;
