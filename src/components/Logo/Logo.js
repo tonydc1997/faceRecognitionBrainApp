841be226208ee99d47e911707e8e5f1076ef962a
Anthony Curtis <tonydc1997@gmail.com>
2018-06-12 08:15:53 -0700

    Make Tilt function less laggy / more responsive

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 65 }} style={{ height: 150, width: 150 }} >
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 65, easing: "cubic-bezier(.03,.98,.52,.99)", speed: 10 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
           <img style={{paddingTop: '5px'}} alt ='logo' src={brain}/>
         </div>
