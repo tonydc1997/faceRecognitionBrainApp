df157b9f821e1b62f7d5698c7069e601128c1fba
Anthony Curtis <tonydc1997@gmail.com>
2018-06-12 08:22:31 -0700

    Create parallax effect

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,9 +6,9 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 65, easing: "cubic-bezier(.03,.98,.52,.99)", speed: 10 }} style={{ height: 150, width: 150 }} >
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 65, easing: "cubic-bezier(.03,.98,.52,.99)", speed: 10 }} style={{ height: 150, width: 150, transformStyle: 'preserve-3d' }} >
         <div className="Tilt-inner pa3">
-          <img style={{paddingTop: '5px'}} alt ='logo' src={brain}/>
+          <img style={{paddingTop: '5px', transform: 'translateZ(20px)'}} alt ='logo' src={brain}/>
         </div>
       </Tilt>
     </div>
