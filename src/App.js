56fc091b395fd129c32b77a6d5c3d2660094fc32
Anthony Curtis <tonydc1997@gmail.com>
2018-07-03 08:12:30 -0700

    Create variables

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
   }
 
   onInputChange = (event) => {
