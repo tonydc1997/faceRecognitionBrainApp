8203654bbc5259e7f614c2088f249b5de7ce02b7
Anthony Curtis <tonydc1997@gmail.com>
2018-07-03 07:50:38 -0700

    Add styles for image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center'>
-      <img alt='' src={imageUrl} />
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img alt='' src={imageUrl} width='500px' height='auto' />
+      </div>
     </div>
-  )
+  );
 }
 
 export default FaceRecognition;
