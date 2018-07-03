297b493b56724be6cd7784dd2f37c7ac4a37ec45
Anthony Curtis <tonydc1997@gmail.com>
2018-07-03 07:44:49 -0700

    Console log bounding box params

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
         this.state.input)
       .then(
       function(response) {
-        console.log(response);
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function(err) {
         // there was an error
