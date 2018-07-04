767177e7bf4b276920ae87b6db8b528abc468c52
Anthony Curtis <tonydc1997@gmail.com>
2018-07-04 09:54:32 -0700

    Change a style

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({ imageUrl }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputImage' alt='' src={imageUrl} width='500px' height='500px' />
+        <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
       </div>
     </div>
   );
