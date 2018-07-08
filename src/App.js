d82b7b0e73d6a683a1f54be4780dd67ccdcb1a91
Anthony Curtis <tonydc1997@gmail.com>
2018-07-08 07:26:05 -0700

    Add some variables

--- src/App.js
+++ src/App.js
@@ -37,6 +37,9 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
