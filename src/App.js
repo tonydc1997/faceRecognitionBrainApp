f5c46c78304ec9939ba34c41d1daa0f4ba6d030c
Anthony Curtis <tonydc1997@gmail.com>
2018-07-08 08:18:37 -0700

    Add some function(s)(ality)

--- src/App.js
+++ src/App.js
@@ -39,8 +39,18 @@ class App extends Component {
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -52,7 +62,7 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
   }
 
