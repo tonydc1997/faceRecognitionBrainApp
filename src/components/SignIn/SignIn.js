dcd94c153a0639e48941ab0e01c0eae71e24370b
Anthony Curtis <tonydc1997@gmail.com>
2018-07-10 11:39:06 -0700

    Fix minor errors

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -25,7 +25,7 @@ const SignIn = ({ onRouteChange }) => {
             />
           </div>
           <div className="lh-copy mt3">
-            <p onClick={() => onRouteChange('register')} className="f6 link dim black db">Register</p>
+            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             {/* <a href="#0" className="f6 link dim black db">Forgot your password?</a> */}
           </div>
         </form>
