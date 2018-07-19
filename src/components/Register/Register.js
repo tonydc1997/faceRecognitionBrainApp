0df9942e434e6b6f1a598e35874f3a60ddef9665
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 07:47:40 -0700

    Update Register Component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
+  }
+  onNameChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+  onPasswordChange = (event) => {
+    this.setState({password: event.target.value});
+  }
+
   render() {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
