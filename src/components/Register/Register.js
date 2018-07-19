ed29dbf7440271df1d7f57415b48961205e5f093
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 07:56:14 -0700

    Add signIn functionality

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -18,9 +18,25 @@ class Register extends React.Component {
   onPasswordChange = (event) => {
     this.setState({password: event.target.value});
   }
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signIn', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data === 'Success') {
+          this.props.onRouteChange('home');
+        }
+      })
+  }
 
   render() {
-    const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -34,7 +50,7 @@ class Register extends React.Component {
                 type="text" 
                 name="name"  
                 id="name" 
-                onChange={onNameChange}
+                onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
@@ -44,7 +60,7 @@ class Register extends React.Component {
                 type="email" 
                 name="email-address"  
                 id="email-address" 
-                onChange={onEmailChange}
+                onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -54,13 +70,13 @@ class Register extends React.Component {
                 type="password" 
                 name="password"  
                 id="password" 
-                onChange={onPasswordChange}
+                onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input 
-                onClick={() => onRouteChange('home')}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                 type="submit" 
                 value="Register" 
