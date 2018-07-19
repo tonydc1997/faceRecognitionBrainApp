c3076318202b6afcc1e747ce690a56d7229f4f63
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 07:52:00 -0700

    Make functions functional within component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,6 +20,7 @@ class Register extends React.Component {
   }
 
   render() {
+    const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -28,15 +29,33 @@ class Register extends React.Component {
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+                <input 
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                type="text" 
+                name="name"  
+                id="name" 
+                onChange={onNameChange}
+                />
               </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <input 
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                type="email" 
+                name="email-address"  
+                id="email-address" 
+                onChange={onEmailChange}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                <input 
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                type="password" 
+                name="password"  
+                id="password" 
+                onChange={onPasswordChange}
+                />
               </div>
             </fieldset>
             <div className="">
