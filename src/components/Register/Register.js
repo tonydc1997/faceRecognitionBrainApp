1f16bfede069ab5cd9df1694f0e348e58e0ce31e
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 08:01:21 -0700

    Add some logic to signIn function

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -31,6 +31,7 @@ class Register extends React.Component {
       .then(response => response.json())
       .then(user => {
         if (user === 'Success') {
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       })
