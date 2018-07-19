b9aaf9078c8f8887f86f46fdb71aa55ca5218e71
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 08:28:57 -0700

    Update rendered components to reflect changes made

--- src/App.js
+++ src/App.js
@@ -107,7 +107,10 @@ class App extends Component {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank />
+              <Rank 
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit} 
@@ -116,7 +119,7 @@ class App extends Component {
             </div>
           : (
             route === 'signIn' 
-            ? <SignIn onRouteChange={this.onRouteChange} />
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
