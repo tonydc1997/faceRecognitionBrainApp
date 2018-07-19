a91b93ec28bc9e45c576f73108c435fd1e070f5d
Anthony Curtis <tonydc1997@gmail.com>
2018-07-19 07:39:41 -0700

    Finish signIn functionality

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -23,7 +23,12 @@ class SignIn extends React.Component {
         password: this.state.signInPassword
       })
     })
-    this.props.onRouteChange('home');
+      .then(response => response.json())
+      .then(data => {
+        if (data === 'Success') {
+          this.props.onRouteChange('home');
+        }
+      })
   }
 
   render() {
