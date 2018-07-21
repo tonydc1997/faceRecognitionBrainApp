ea68f506b3963fcb265a8e82ae454240640f0ede
Anthony Curtis <tonydc1997@gmail.com>
2018-07-21 07:11:39 -0700

    Update user profile response

--- src/App.js
+++ src/App.js
@@ -83,7 +83,16 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3'>
-        {`${name}, your current rank is ...`}
+      <div className='white f1'>
+        {`${name}, your current entry count is ...`}
       </div>
       <div className='white f1'>
         {entries}
