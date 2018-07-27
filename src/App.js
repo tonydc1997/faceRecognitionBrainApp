5aa2b155ad53f0769cf86d303f002ce633e8f984
Anthony Curtis <tonydc1997@gmail.com>
2018-07-27 07:54:29 -0700

    Move clarifai info to back-end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
@@ -9,10 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: 'YOUR_API_KEY'
- });
 
 const particlesOptions = {
   particles: {
@@ -81,10 +76,6 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
