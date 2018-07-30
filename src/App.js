896e4088ddde1f0cef2c487212ef5bc53bd193ac
Anthony Curtis <tonydc1997@gmail.com>
2018-07-30 07:51:24 -0700

    Fix smalll error

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://powerful-depths-38914.herokuapp.com:3000/image', {
+        fetch('https://powerful-depths-38914.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
