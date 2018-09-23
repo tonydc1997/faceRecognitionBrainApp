ba4514796c654a97684411f33db524a81f709125
Anthony Curtis <tonydc1997@gmail.com>
2018-09-23 10:46:19 -0700

    Align form field text for cleaner look

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -45,7 +45,7 @@ class Register extends React.Component {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <label className="db fw6 lh-copy f6 fl w-10" htmlFor="name">Name</label>
                 <input 
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="text" 
@@ -55,7 +55,7 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="db fw6 lh-copy f6 fl w-10" htmlFor="email-address">Email</label>
                 <input 
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="email" 
@@ -65,7 +65,7 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="db fw6 lh-copy f6 fl w-10" htmlFor="password">Password</label>
                 <input 
                 className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="password" 
