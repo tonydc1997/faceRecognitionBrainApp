b9f98f9ed6dc50b96f2158f3e740a183704bc3ab
Anthony Curtis <tonydc1997@gmail.com>
2018-09-23 10:29:45 -0700

    Create responsive curved box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <div className='bounding-box br3' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     </div>
   );
