d0d5b61d83ad32dfa5d291470477b1f69a5314d7
Anthony Curtis <tonydc1997@gmail.com>
2018-09-23 17:57:58 -0700

    Update style to make form stand out

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -37,7 +37,7 @@ class SignIn extends React.Component {
     const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+        <main className="pa4 black-80 ba bw1 br3 b--mid-gray">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
