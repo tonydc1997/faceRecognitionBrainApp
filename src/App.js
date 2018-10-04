b9b59b0c06c7343842ee5987356d791576d6ed79
Anthony Curtis <tonydc1997@gmail.com>
2018-10-04 12:30:14 -0700

    Move code into mapping function

--- src/App.js
+++ src/App.js
@@ -55,16 +55,16 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     return data.outputs[0].data.regions.map(face => {
       const clarifaiFace = face.region_info.bounding_box;
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
     });
-    const image = document.getElementById('inputImage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
   }
 
   displayFaceBox = (box) => {
