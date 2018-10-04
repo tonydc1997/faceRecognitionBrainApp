4c0280c2847928bfdcf1d1d65d3f49b67f6ad4d8
Anthony Curtis <tonydc1997@gmail.com>
2018-10-04 12:29:13 -0700

    Map over faces

--- src/App.js
+++ src/App.js
@@ -53,7 +53,9 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace = data.outputs[0].data.regions.map(face => {
+      face.region_info.bounding_box;
+    });
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
