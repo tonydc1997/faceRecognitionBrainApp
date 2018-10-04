38441b678e350ebde222b448e1aab3b3157e819c
Anthony Curtis <tonydc1997@gmail.com>
2018-10-04 12:50:50 -0700

    Create a key prop

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -8,7 +8,7 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
         <img id='inputImage' alt='' src={imageUrl} width='300px' height='auto' className="br2"/>
         {
           boxes.map(box => {
-            return <div className='bounding-box br3' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            return <div key={box.topRow} className='bounding-box br3' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
           })
         }
       </div>
