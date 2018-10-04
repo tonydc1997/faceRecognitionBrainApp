693da71de1c8878d30fe477f04af44c1c9de3bc2
Anthony Curtis <tonydc1997@gmail.com>
2018-10-04 12:38:54 -0700

    Rename box to reflect update

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputImage' alt='' src={imageUrl} width='300px' height='auto' className="br2"/>
-        <div className='bounding-box br3' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <div className='bounding-box br3' style={{top: boxes.topRow, right: boxes.rightCol, bottom: boxes.bottomRow, left: boxes.leftCol}}></div>
       </div>
     </div>
   );
