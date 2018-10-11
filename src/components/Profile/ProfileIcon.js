10a554d2f606e235187a34c3812b916b9c533952
Anthony Curtis <tonydc1997@gmail.com>
2018-10-11 13:42:26 -0700

    Create initial state

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class ProfileIcon extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropDownOpen: false
+    }
+  }
+
+  render() {
+    return (
+      
+    );
+  }
 }
 
 export default ProfileIcon;
