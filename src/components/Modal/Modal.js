a74ff88074e573233e0307924e8a3412f08c5ff6
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 11:32:10 -0700

    Bind modal-root to component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './Modal.css';
 
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+  
+}
+
 
 export default Modal;
