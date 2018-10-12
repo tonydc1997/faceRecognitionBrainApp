923a199b3757fd7c39a9f7b30e34078f58ee6945
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 11:35:04 -0700

    Fix spelling mistake

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './Modal.css';
 
 const modalRoot = document.getElementById('modal-root');
@@ -10,7 +10,7 @@ class Modal extends React.Component {
     this.element = document.createElement('div');
   }
 
-  
+
 }
 
 
