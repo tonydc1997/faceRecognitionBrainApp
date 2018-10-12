47691165bb869dc69af5d60c3210399bdc1c8d63
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 11:02:39 -0700

    Import and use hover style

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SignIn.css';
 // import { userInfo } from 'os';
 
 class SignIn extends React.Component {
@@ -45,7 +46,7 @@ class SignIn extends React.Component {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6 tl" htmlFor="email-address">Email</label>
                 <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
                   type="email" 
                   name="email-address"  
                   id="email-address"
@@ -55,7 +56,7 @@ class SignIn extends React.Component {
               <div className="mv3">
                 <label className="db fw6 lh-copy f6 tl" htmlFor="password">Password</label>
                 <input 
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
                 type="password" 
                 name="password"  
                 id="password" 
