0b9ca7076a1c3270f6d85942942d6bccaa6e795e
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 11:34:27 -0700

    Allow Modal component access to props

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,6 +5,11 @@ import './Modal.css';
 const modalRoot = document.getElementById('modal-root');
 
 class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.element = document.createElement('div');
+  }
+
   
 }
 
