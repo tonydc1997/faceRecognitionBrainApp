6f81e13d648a518ffb55f1f7c90931ac0d3a7763
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 13:13:40 -0700

    Remove unneeded content

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -14,47 +14,28 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
           <p>Member since: TIMESTAMP</p>
           <hr/>
           <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6 tl" htmlFor="name">Name</label>
-                <input 
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                type="text" 
-                name="name"  
-                id="name" 
-                onChange={this.onNameChange}
-                />
-              </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6 tl" htmlFor="email-address">Email</label>
-                <input 
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                type="email" 
-                name="email-address"  
-                id="email-address" 
-                onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6 tl" htmlFor="password">Password</label>
-                <input 
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
-                type="password" 
-                name="password"  
-                id="password" 
-                onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div className="">
-              <input 
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                type="submit" 
-                value="Register" 
-              />
-            </div>
+            <label className="db fw6 lh-copy f6 tl" htmlFor="name">Name</label>
+            <input 
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+              type="text" 
+              name="name"  
+              id="name" 
+              onChange={this.onNameChange}
+            />
+            <input 
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+              type="email" 
+              name="email-address"  
+              id="email-address" 
+              onChange={this.onEmailChange}
+            />
+            <input 
+              className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+              type="password" 
+              name="password"  
+              id="password" 
+              onChange={this.onPasswordChange}
+            />
           </div>
         </main>
       </article>
