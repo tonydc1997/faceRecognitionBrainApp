5e7ade543b4c84d8f49f5843f96817d6daf9ba37
Anthony Curtis <tonydc1997@gmail.com>
2018-10-12 13:00:18 -0700

    Add and fix profile styling

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import './Profile.css';
 
 const Profile = ({ isProfileOpen, toggleModal }) => {
   return (
-    <div className="br3 ba b-black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
+    <div className="profile-modal">
       <button onClick={toggleModal} >Click</button>
     </div>
   );
