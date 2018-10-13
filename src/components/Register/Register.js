3112399428a49f4f93f01cac0b20f7babc36e8b9
Anthony Curtis <tonydc1997@gmail.com>
2018-10-13 07:07:00 -0700

    Remove unneccessary css file and associated styles

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -49,7 +49,7 @@ class Register extends React.Component {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6 tl" htmlFor="name">Name</label>
                 <input 
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="text" 
                 name="name"  
                 id="name" 
@@ -59,7 +59,7 @@ class Register extends React.Component {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6 tl" htmlFor="email-address">Email</label>
                 <input 
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="email" 
                 name="email-address"  
                 id="email-address" 
@@ -69,7 +69,7 @@ class Register extends React.Component {
               <div className="mv3">
                 <label className="db fw6 lh-copy f6 tl" htmlFor="password">Password</label>
                 <input 
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                 type="password" 
                 name="password"  
                 id="password" 
