f13d9e0afd7d192a826f18fbfc0b2dfd017ef9a9
Anthony Curtis <tonydc1997@gmail.com>
2018-10-15 15:38:31 -0700

    Remove commented code

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,11 +1,4 @@
 import React from 'react';
-// import './Profile.css';
-// import { 
-//   Dropdown, 
-//   DropdownToggle, 
-//   DropdownMenu, 
-//   DropdownItem 
-// } from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
@@ -50,33 +43,5 @@ class ProfileIcon extends React.Component {
     );
   }
 }
-
-      {/* <div className="pa4 tc">
-        // Dropdown
-        <div isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          //Dropdown Toggle
-          <div
-            tag="span"
-            data-toggle="dropdown"
-            aria-expanded={this.state.dropdownOpen}
-          >
-          <img
-            src="http://tachyons.io/img/logo.jpg"
-            className="br-100 ba h3 w3 dib" alt="avatar" />
-          </div>
-          //Dropdown Menu
-          <div
-            right
-            className="b--transparent shadow-5" 
-            style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}
-          > // Dropdown Item
-            <div onClick={this.props.toggleModal}>View Profile</div>
-            // Dropdown Item
-            <div divider />
-            // Dropdown Item
-            <div onClick={() => this.props.onRouteChange('signOut')}>Sign Out</div>
-          </div>
-        </div>
-      </div> */}
 
 export default ProfileIcon;
