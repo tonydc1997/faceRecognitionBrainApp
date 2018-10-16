98f9a8ea69213d43b9452e6c6c4c030d130f49d1
Anthony Curtis <tonydc1997@gmail.com>
2018-10-16 10:32:56 -0700

    Turn stateless component into one that contains state

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,52 +1,62 @@
 import React from 'react';
 import './Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal }) => {
-  return (
-    <div className="profile-modal">
-      <article className="br3 ba bw1 b--black-60 mv4 w-100 w-70-m w-35-l mw6 shadow-5 center bg-white">
-        <main className="pa4 black-80 w-80">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          className="h3 w3 dib" alt="avatar" />
-          <h1>Shovel Knight</h1>
-          <h4>Images Submitted: NUMBER</h4>
-          <p>Member since: TIMESTAMP</p>
-          <hr/>
-          <label className="mt2 fw6" htmlFor="user-name">Name:</label>
-          <input 
-            className="pa2 ba w-100" 
-            type="text" 
-            placeholder="Shovel Knight"
-            name="user-name"  
-            id="name" 
-          />
-          <label className="mt2 fw6" htmlFor="user-age">Age:</label>
-          <input 
-            className="pa2 ba w-100" 
-            type="text" 
-            placeholder="90"
-            name="user-age"  
-            id="age" 
-          />
-          <label className="mt2 fw6" htmlFor="user-book">Favorite Book:</label>
-          <input 
-            className="pa2 ba w-100" 
-            type="text" 
-            placeholder="Deep Work"
-            name="user-book"  
-            id="book" 
-          />
-          <div className="mt2" style={{ display: 'flex', justifyContent: 'space-evenly'}}>
-            <button className="ba pa2 grow pointer w-40 bg-blue b--dark-blue br1 white">Save</button>
-            <button className="ba pa2 grow pointer w-40 bg-white b--dark-red br1 red"
-            onClick={toggleModal}>Cancel</button>
-          </div>
-        </main>
-        <div className="modal-close" onClick={toggleModal}>&times;</div>
-      </article>
-    </div>
-  );
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.user.name,
+      age: this.props.user.age,
+      book: this.props.user.book
+    }
+  }
+  render() {
+    return (
+      <div className="profile-modal">
+        <article className="br3 ba bw1 b--black-60 mv4 w-100 w-70-m w-35-l mw6 shadow-5 center bg-white">
+          <main className="pa4 black-80 w-80">
+          <img
+            src="http://tachyons.io/img/logo.jpg"
+            className="h3 w3 dib" alt="avatar" />
+            <h1>Shovel Knight</h1>
+            <h4>Images Submitted: NUMBER</h4>
+            <p>Member since: TIMESTAMP</p>
+            <hr/>
+            <label className="mt2 fw6" htmlFor="user-name">Name:</label>
+            <input 
+              className="pa2 ba w-100" 
+              type="text" 
+              placeholder="Shovel Knight"
+              name="user-name"  
+              id="name" 
+            />
+            <label className="mt2 fw6" htmlFor="user-age">Age:</label>
+            <input 
+              className="pa2 ba w-100" 
+              type="text" 
+              placeholder="90"
+              name="user-age"  
+              id="age" 
+            />
+            <label className="mt2 fw6" htmlFor="user-book">Favorite Book:</label>
+            <input 
+              className="pa2 ba w-100" 
+              type="text" 
+              placeholder="Deep Work"
+              name="user-book"  
+              id="book" 
+            />
+            <div className="mt2" style={{ display: 'flex', justifyContent: 'space-evenly'}}>
+              <button className="ba pa2 grow pointer w-40 bg-blue b--dark-blue br1 white">Save</button>
+              <button className="ba pa2 grow pointer w-40 bg-white b--dark-red br1 red"
+              onClick={toggleModal}>Cancel</button>
+            </div>
+          </main>
+          <div className="modal-close" onClick={toggleModal}>&times;</div>
+        </article>
+      </div>
+    );
+  }
 }
 
 export default Profile;
