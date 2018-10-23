c90222786597a3915c54110c13fb6abe318c8b28
Anthony Curtis <tonydc1997@gmail.com>
2018-10-23 07:41:06 -0700

    style: Make button font bold for better readability

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -14,7 +14,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => (
           onChange={onInputChange}
         />
         <button
-          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple b"
           onClick={onButtonSubmit}
         >
           Detect
