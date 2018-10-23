4680027ce8934ede10c6e0ac65613a2cfc9b39c5
Anthony Curtis <tonydc1997@gmail.com>
2018-10-23 12:51:32 -0700

    fix: Load user correctly upon initial Sign-In
    - Without token, users were undefined. With token, users were identified
    
    - GET profile via userId
    
    - Assign token
    
    - Load correct user based on the user info one receives

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -36,8 +36,20 @@ class SignIn extends React.Component {
       .then(data => {
         if (data.userId && data.success === 'true') {
           this.saveAuthTokenInSession(data.token);
-          this.props.loadUser(data);
-          this.props.onRouteChange('home');
+          fetch(`http://localhost:3000/profile/${data.userId}`, {
+            method: 'get',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': data.token,
+            },
+          })
+            .then(response => response.json())
+            .then(user => {
+              if (user && user.email) {
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
+              }
+            });
         }
       });
   };
