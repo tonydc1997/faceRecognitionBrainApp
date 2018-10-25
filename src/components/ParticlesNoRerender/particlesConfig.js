198db90e61f0f06e4382d802c4a8439d7222b1de
Anthony Curtis <tonydc1997@gmail.com>
2018-10-25 10:14:57 -0700

    test: Test out different values for particlesConfig

--- src/components/ParticlesNoRerender/particlesConfig.js
+++ src/components/ParticlesNoRerender/particlesConfig.js
@@ -1,10 +1,10 @@
 const particlesConfig = {
   particles: {
     number: {
-      value: 30,
+      value: 900,
       density: {
         enable: true,
-        value_area: 800,
+        value_area: 10,
       },
     },
   },
