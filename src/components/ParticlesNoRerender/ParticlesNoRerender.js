ac301d1c904c5bb68986e7b49269225d6f7a5533
Anthony Curtis <tonydc1997@gmail.com>
2018-10-25 09:36:50 -0700

    feat: Convert Particles into smart component, controll it's update cycle

--- src/components/ParticlesNoRerender/ParticlesNoRerender.js
+++ src/components/ParticlesNoRerender/ParticlesNoRerender.js
@@ -1,3 +1,17 @@
 import React, { Component } from "react";
 import Particles from 'react-particles-js';
 import particlesConfig from './particlesConfig';
+
+class ParticlesNoRerender extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) => {
+    return false;
+  }
+
+  render( {
+    return (
+      <Particles />
+    )
+  })
+}
+
+export default ParticlesNoRerender;
