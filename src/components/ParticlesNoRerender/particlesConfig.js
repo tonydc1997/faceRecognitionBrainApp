f67f7591bb38dbde43c34d86df9afb9f056e6b9d
Anthony Curtis <tonydc1997@gmail.com>
2018-11-02 10:29:37 -0700

    refactor: Remove unused params and import statement in particlesConfig

--- src/components/ParticlesNoRerender/particlesConfig.js
+++ src/components/ParticlesNoRerender/particlesConfig.js
@@ -1,7 +1,7 @@
 const particlesConfig = {
   particles: {
     number: {
-      value: 160,
+      value: 12,
       density: {
         enable: true,
         value_area: 800,
@@ -21,7 +21,7 @@ const particlesConfig = {
       },
     },
     opacity: {
-      value: 1,
+      value: 0.6,
       random: true,
       anim: {
         enable: true,
@@ -31,7 +31,7 @@ const particlesConfig = {
       },
     },
     size: {
-      value: 3,
+      value: 6,
       random: true,
       anim: {
         enable: false,
@@ -43,9 +43,6 @@ const particlesConfig = {
     line_linked: {
       enable: false,
       distance: 150,
-      color: '#ffffff',
-      opacity: 0.4,
-      width: 1,
     },
     move: {
       enable: true,
@@ -59,45 +56,6 @@ const particlesConfig = {
         enable: false,
         rotateX: 600,
         rotateY: 600,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: 'canvas',
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'bubble',
-      },
-      onclick: {
-        enable: true,
-        mode: 'repulse',
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 250,
-        size: 0,
-        duration: 2,
-        opacity: 0,
-        speed: 3,
-      },
-      repulse: {
-        distance: 400,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
       },
     },
   },
