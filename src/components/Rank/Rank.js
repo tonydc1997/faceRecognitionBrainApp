b0e5ee49c679b7bade02decb17540c05863f4888
Anthony Curtis <tonydc1997@gmail.com>
2018-11-11 07:11:31 -0800

    style: Fix alignment for entries div and add ' ; ' in Rank.js via eslint

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -14,11 +14,9 @@ class Rank extends React.Component {
         <div className="white f3">
           {`${name}, your current entry count is ...`}
         </div>
-        <div className="white f1">
-          {entries}
-        </div>
+        <div className="white f1">{entries}</div>
       </div>
-    )
+    );
   }
 }
 export default Rank;
