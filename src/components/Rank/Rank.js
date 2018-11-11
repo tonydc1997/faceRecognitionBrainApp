7acd26b2f70b9c5458a40f02eb912c6e4a04b3ef
Anthony Curtis <tonydc1997@gmail.com>
2018-11-11 07:08:00 -0800

    feat: Change stateless Rank.js into stateful one

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => (
-  <div>
-    <div className="white f3">{`${name}, your current entry count is ...`}</div>
-    <div className="white f1">{entries}</div>
-  </div>
-);
-
+class Rank extends React.Component { 
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${name}, your current entry count is ...`}
+        </div>
+        <div className="white f1">
+          {entries}
+        </div>
+      </div>
+    )
+  }
+}
 export default Rank;
