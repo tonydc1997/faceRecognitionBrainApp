7d11f4dd59628b9e7f11a9cec78faa64f5c9daae
Anthony Curtis <tonydc1997@gmail.com>
2018-11-12 07:05:05 -0800

    fix: Remove handleResponse() from fetch call in Rank.js
    - handleResponse() gave me undefined value when I received data.input.
    
    - Replace above fn() with usual 'resp => resp.json()' to resolve issue.

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -14,11 +14,11 @@ class Rank extends React.Component {
   }
 
   generateEmoji = entries => {
-    const { handleResponse, errorLog } = this.props;
+    const { errorLog } = this.props;
     fetch(
       `https://j8aypdmm19.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`,
     )
-      .then(handleResponse)
+      .then(response => response.json())
       .then(data => this.setState({ emoji: data.input }))
       .catch(errorLog);
   };
