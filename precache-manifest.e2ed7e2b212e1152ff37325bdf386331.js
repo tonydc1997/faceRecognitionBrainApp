e049c41afb495d6ebf47fe3945308d077802d4db
Anthony Curtis <tonydc1997@gmail.com>
2018-12-06 07:38:15 -0800

    Updates

---
+++ precache-manifest.e2ed7e2b212e1152ff37325bdf386331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dacf23b9529adb55d48",
+    "url": "/faceRecognitionBrainApp/static/js/runtime~main.8dacf23b.js"
+  },
+  {
+    "revision": "4cbd822513ad32f07691",
+    "url": "/faceRecognitionBrainApp/static/js/main.4cbd8225.chunk.js"
+  },
+  {
+    "revision": "6931582c8246eaee73f2",
+    "url": "/faceRecognitionBrainApp/static/js/1.6931582c.chunk.js"
+  },
+  {
+    "revision": "4cbd822513ad32f07691",
+    "url": "/faceRecognitionBrainApp/static/css/main.debab79a.chunk.css"
+  },
+  {
+    "revision": "6931582c8246eaee73f2",
+    "url": "/faceRecognitionBrainApp/static/css/1.75d2c91b.chunk.css"
+  },
+  {
+    "revision": "7f49344f2d46051d34e9f89bc6fc4b11",
+    "url": "/faceRecognitionBrainApp/index.html"
+  }
+];
