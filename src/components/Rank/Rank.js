d8cfa25c90a1ef577a5f37948bcbf11540ed5b1e
Anthony Curtis <tonydc1997@gmail.com>
2020-04-09 09:24:18 -0400

    feat: Replace lifecycle methods with react hook
    - Replace componentDidMount/DidUpdate w/  useEffect.
    
    - With useEffect, generateEmojis according to entries so long as entries updates.

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-const Rank = ({entries, name, errorLog}) => {
+const Rank = ({ entries, name, errorLog }) => {
   const [emojiRank, setEmoji] = useState({
     emoji: '',
-  })
+  });
   const { emoji } = emojiRank;
 
   const generateEmoji = entries => {
@@ -15,27 +15,19 @@ const Rank = ({entries, name, errorLog}) => {
       .catch(errorLog);
   };
 
-  componentDidMount() {
-    this.generateEmoji(entries);
-  }
+  useEffect(() => {
+    generateEmoji(entries);
+  }, [entries]);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.entries === entries && prevProps.name === name) {
-      return null;
-    }
-    this.generateEmoji(entries);
-  }
-
-    return (
-      <div>
-        <div className="white f3">
-          {`${name}, your current entry count is ...`}
-        </div>
-        <div className="white f1">{entries}</div>
-        <div className="white f3">{`Rank Badge: ${emoji}`}</div>
+  return (
+    <div>
+      <div className="white f3">
+        {`${name}, your current entry count is ...`}
       </div>
-    );
-  }
-
+      <div className="white f1">{entries}</div>
+      <div className="white f3">{`Rank Badge: ${emoji}`}</div>
+    </div>
+  );
+};
 
 export default Rank;
